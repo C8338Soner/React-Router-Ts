dd6cc8a71620e75ca466cbbdc4043e191b9eac60
SONER PEKER <80073225+C8338Soner@users.noreply.github.com>
2023-11-22 22:37:34 +0300

    initial

---
+++ Router1Exercise/router-ts/src/index.tsx
@@ -0,0 +1,33 @@
+import * as React from "react";
+import * as ReactDOM from "react-dom/client";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import "./index.css";
+import Root from "./routes/root";
+import ErrorPage from "./errorpage";
+import Contact from "./routes/rootcontact";
+
+const rootElement = document.getElementById("root");
+
+if (rootElement) {
+  const router = createBrowserRouter([
+    {
+      path: "/",
+      element: <Root />,
+      errorElement: <ErrorPage />,
+      children: [
+        {
+          path: "/contacts/:contactId",
+          element: <Contact />,
+        },
+      ],
+    },
+  ]);
+
+  ReactDOM.createRoot(rootElement).render(
+    <React.StrictMode>
+      <RouterProvider router={router} />
+    </React.StrictMode>
+  );
+} else {
+  console.error("Root element with ID 'root' not found.");
+}
